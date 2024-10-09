7dc5659d29f7b9c870b9d0765a5e991c04dfeab1
PatrickMontes <alexandermontes30p@hotmail.com>
2024-10-09 18:31:14 -0500

    Producto Endpoint

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -40,7 +40,62 @@ export default class ApiService{
 
 
     //*** Producto ENDPOINT***//
+    static async agregarProducto(formData){
+        const response = await axios.post(`${this.BASE_URL}/producto/crear`, formData, {
+            headers: {
+                ...this.getHeader(),
+                "Content-Type": "multipart/form-data"
+            }
+        })
+        return response.data
+    }
+
+
+    static async actualizar(productoId, formData){
+        const response = await axios.put(`${this.BASE_URL}/producto/actualizar/${productoId}`, formData, {
+            headers: {
+                ...this.getHeader(),
+                "Content-Type": "multipart/form-data"
+            }
+        })
+        return response.data
+    }
+
+
+    static async getAllProductos(){
+        const response = await axios.get(`${this.BASE_URL}/producto/getAll`)
+        return response.data
+    }
+
+
+    static async buscar(valor){
+        const response = await axios.get(`${this.BASE_URL}/producto/buscar`, {
+            params: (valor)
+        })
+        return response.data
+    }
+
+
+    static async getAllProductosPorCategoriaId(categoriaId){
+        const response = await axios.get(`${this.BASE_URL}/producto/getProductosPorCategoria/${categoriaId}`)
+        return response.data
+    }
+
+
+    static async getAllProductosPorId(productoId){
+        const response = await axios.get(`${this.BASE_URL}/producto/getProductoPorId/${productoId}`)
+        return response.data
+    }
+
+
+    static async eliminarProducto(productoId){
+        const response = await axios.delete(`${this.BASE_URL}/producto/eliminar/${productoId}`, {
+            headers: this.getHeader()
+        })
+        return response.data
+    }
     
 
 
+    //*** Categoria ENDPOINT***//
 }
